70f57d60bbe277ad19a8f4b869a7bb1619da6490
Edgar Toll <edjopato@gmail.com>
2017-08-11 01:53:58 +0200

    fix: typo

--- ics.php
+++ ics.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>HAW Hamburg Kalendar Bot</title>
+  <title>HAW Hamburg Kalender Bot</title>
   <meta name="format-detection" content="telephone=no">
   <style type="text/css">
     body {
