1232e9340ba1003184575d2b3be0aeb071e18c03
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-01-05 11:40:03 +0100

    style: switch to tabs

--- ics.js
+++ ics.js
@@ -2,10 +2,10 @@ document.getElementById('dateNumber').innerHTML = new Date().getDate()
 
 const pair = window.location.search.slice(1).split('=');
 if (pair[0] === 'url') {
-  const url = 'webcal://' + pair[1]
-  const urlElement = document.getElementById('calurl')
-  urlElement.setAttribute('href', url)
-  console.log(url, urlElement);
+	const url = 'webcal://' + pair[1]
+	const urlElement = document.getElementById('calurl')
+	urlElement.setAttribute('href', url)
+	console.log(url, urlElement);
 } else {
-  console.error('url in query is missing!')
+	console.error('url in query is missing!')
 }
